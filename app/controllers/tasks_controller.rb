cfc8be85d62d78ad38c34322e162633420fe2cc8
kesleymartins <kesley.martins@vivaldi.net>
2022-04-26 20:57:35 -0300

    add form to create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,20 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: 'task successfully created'
+    else
+      redirect_to root_path, alert: 'an error occurred while creating the task'
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
