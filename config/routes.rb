f5ad7695e38974872903e3934c803d40f6ed28e9
kesleymartins <kesley.martins@vivaldi.net>
2022-04-26 22:50:58 -0300

    create home and about static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'tasks#index'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+
   put '/tasks/:id', to: 'tasks#complete', as: 'complete_task'
-  resources :tasks, only: %i[ create destroy ]
+  resources :tasks, only: %i[ index create destroy ]
 end
