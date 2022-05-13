8e240feaba013c0ba948dcf490f1ee18e88d8c07
kesleymartins <kesley.martins@vivaldi.net>
2022-05-13 19:32:21 -0300

    add animation when flashmessage in and timeou to close

--- app/javascript/controllers/flashmessage_controller.js
+++ app/javascript/controllers/flashmessage_controller.js
@@ -1,6 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    this.element.classList.add('xyz-in')
+  
+    setTimeout(() => {
+      this.close()
+    }, 5000);
+  }
+
   close() {
     this.element.style.display = "none"
   }
